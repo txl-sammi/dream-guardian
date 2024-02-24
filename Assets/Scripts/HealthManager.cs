378b86fe356554372362a798be209640a89318f5
Rosa Yan <rosa012933@gmail.com>
2024-02-25 03:28:58 +1100

    procrastinating on my assignment

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// Based on HealthManager.cs from https://github.com/COMP30019/Workshop-4-Solution 
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] public int startingHealth;
+    [SerializeField] public float healthChange;
+    [SerializeField] private UnityEvent onDeath;
+    [SerializeField] public UnityEvent onHealthChanged;
+    [SerializeField] public UnityEvent onDamageTaken;
+
+    public float _currentHealth;
+    public float CurrentHealth
+    {
+        get
+        {
+            return _currentHealth;
+        }
+        set
+        {
+            _currentHealth = value;
+            onHealthChanged.Invoke();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetHealth();
+    }
+
+    public void ResetHealth()
+    {
+        CurrentHealth = this.startingHealth;
+    }
+
+    public void Heal(int healAmount)
+    {
+        if (CurrentHealth + healAmount > startingHealth)
+        {
+            CurrentHealth = startingHealth;
+        }
+        else
+        {
+            CurrentHealth += healAmount;
+            //onHealthChanged.Invoke();
+        }
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        CurrentHealth -= damage;
+        onDamageTaken.Invoke();
+    }
+
+
+    void Update()
+    {
+        CurrentHealth -= healthChange * Time.deltaTime;
+        if (_currentHealth <= 0 && onDeath != null)
+        {
+            onDeath.Invoke();
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5;
+    [SerializeField] private float moveSpeed = 5;
+    [SerializeField] private float interactRange = 1; 
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
 
@@ -90,5 +91,11 @@ public class Player : MonoBehaviour
         }
         inputVector = inputVector.normalized;
         return inputVector;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, interactRange);
     }
 }
